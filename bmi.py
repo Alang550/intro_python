3408e410799ca6a3b3b19b058005d7e73dab89d9
alang <alangdorcas@gmail.com>
2025-03-17 05:59:18 -0400

    intro_python

---
+++ bmi.py
@@ -0,0 +1,17 @@
+#BMI calc
+weight = 63
+height = 1.75
+bmi = weight / height
+
+if bmi < 18.5:
+    print("underweight")
+elif bmi >= 18.5 and bmi <= 24.9:
+     print("normal")
+elif bmi >= 25 and bmi <= 29.9:
+   print("overweight")
+elif bmi >= 30 and bmi <= 34.9:
+    print("obese")
+elif bmi >= 35 and bmi <= 39.9:
+    print("obese")
+else:
+    print("morbidly obese")
---
+++ data_types.py
@@ -0,0 +1,6 @@
+ #data types
+age = 20 # integer
+weight = 60.5 # float
+names ="shang tiang" # string
+is_listed = True # boolean
+ 
